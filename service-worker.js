e8156b2b15296c7061cabc4c208c6b7c21bd3a43
Jacob Ebey <v-jaebe@microsoft.com>
2018-11-26 02:44:49 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hooks-todo/precache-manifest.0dfa2e32ef597647e6b4713f8ee29688.js"
+  "/hooks-todo/precache-manifest.ec4b740d42944b98184f3860b3c03de8.js"
 );
 
 workbox.clientsClaim();
