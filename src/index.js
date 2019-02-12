c0809ab12053a10cca11068d6a00f9bd4a59e26a
Jacob Ebey <jacob-ebey@live.com>
2019-02-12 15:24:15 -0800

    - Updated to the official hooks release

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from "./App/App";
 import * as serviceWorker from "./serviceWorker";
 
 const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.render(<App />, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
